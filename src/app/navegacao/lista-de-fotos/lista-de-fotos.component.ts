91dff55015000cf708ed3b3b16feb17833f3fded
Gabriel Pulga <gabriel.pulga@codefiction.net>
2021-01-07 08:55:20 -0300

    last changes

---
+++ src/app/navegacao/lista-de-fotos/lista-de-fotos.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-fotos',
+  template: `
+    <p>
+      lista-de-fotos works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ListaDeFotosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/navegacao/lista-de-usuarios/lista-de-usuarios.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-usuarios',
+  templateUrl: './lista-de-usuarios'
+})
+export class ListaDeUsuariosComponent{}
