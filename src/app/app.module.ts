234751f5bb753cbbcdf3d5296e1216e421e09b0d
Gabriel Pulga <gabriel.pulga@codefiction.net>
2021-01-07 08:55:01 -0300

    last changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { LoginComponent } from './navegacao/login/login.component';
+import { ListaDeFotosComponent } from './navegacao/lista-de-fotos/lista-de-fotos.component';
+import { ListaDeUsuariosComponent } from './navegacao/lista-de-usuarios/lista-de-usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginComponent,
+    ListaDeFotosComponent,
+    ListaDeUsuariosComponent
   ],
   imports: [
     BrowserModule
